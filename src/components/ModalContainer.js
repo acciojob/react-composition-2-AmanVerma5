71f274cf4ed6fea899332ed5cec3de76d54bd3df
AmanVerma5 <amanv4775@gmail.com>
2023-08-01 18:41:40 +0530

    submitting code-Tue Aug 01 2023 18:41:39 GMT+0530 (India Standard Time)

--- src/components/ModalContainer.js
+++ src/components/ModalContainer.js
@@ -10,7 +10,7 @@ const ModalContainer=({handleClick})=>{
     return(
         <div className="model-overlay">
             <button className="model-close" onClick={()=>handleClick(false)}>Close</button>
-            <p className="model-p">This is the content of the modal</p>
+            <p className="model p">This is the content of the modal</p>
         </div>
     )
 }
